c5642a0fa67d3cadc9d2378830c81b7b71472431
atl18 <atl18@mails.tsinghua.edu.cn>
2019-04-02 23:37:16 +0800

    knn initialize

--- knn.py
+++ knn.py
@@ -3,3 +3,9 @@ class knn:
         if train_data is not None:
             self.data = train_data
         return
+
+    def set_k(self, k):
+        self.k = k
+
+    def __normalize(self):
+        max(self.data)
